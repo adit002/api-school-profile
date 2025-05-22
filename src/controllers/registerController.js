ddf922563eb51ddb5b26b0c3898dec3fb394f859
aditya rizky putra <adityarizkyputra@gmail.com>
2025-05-23 05:09:58 +0700

    update

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -21,20 +21,26 @@ controller.list = (req, res) => {
 };
 
 controller.save = (req, res) => {
-    const data = req.body;
+  const data = req.body;
 
-    req.getConnection((err, conn) => {
-        conn.query('INSERT INTO register set ?', [data], (err, register) => {
-            if (err) {
-                res.json(err);
-            }
-            res.status(200).json({
-                message: 'Success',
-                response: 200,
-            });
-        });
+  req.getConnection((err, conn) => {
+    if (err) {
+      return res.status(500).json({ message: 'Database connection error', error: err });
+    }
+    conn.query('INSERT INTO register SET ?', [data], (err, result) => {
+      if (err) {
+        return res.status(500).json({ message: 'Insert failed', error: err });
+      }
+
+      return res.status(200).json({
+        message: 'Success',
+        response: 200,
+        insertId: result.insertId, 
+      });
     });
+  });
 };
+
 
 controller.update = (req, res) => {
     const { id, ...newregister } = req.body;
